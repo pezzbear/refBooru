db2309ad287f18e025c8da389823a4dce75997b0
Edan Deno <edanzdeno@gmail.com>
2023-10-27 14:59:36 -0500

    Lots of stuff. Working on database rn

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+
+<script>
+    import '../css/main.css';    
+    import '../css/stylesheet.css';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+</script>
+
+<div class="set-font">
+    <!--Navbar / Banner-->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-link" href="/">
+            <img src="refBooruIcon.svg" alt="refBooru icon">
+        </a>
+        <a class="navbar-link-text text-center" href="/">
+            Home
+        </a>
+        <a class="navbar-link-text text-center" href="/about">
+            About
+        </a>
+        <a class="navbar-link-text text-center" href="/contact">
+            Contact
+        </a>
+    </nav>
+
+    <slot />
+</div>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,22 @@
-<script> import 'bootstrap/dist/css/bootstrap.min.css' </script>
+<div class="container-fluid">
+    <div class="row">
+        <!-- SideBar Search and Tag Layout -->
+        <nav class="col-md-3 d-md-block sidebar">
+            <div class="position-sticky form-content">
+                <!-- Search -->  
+                <h5 id="search-for-images">Search for Images</h5>
+                <form class="search-bar text-center" method="post">
+                    <input name="search" autocomplete="off">
+                    <button type="button">Search</button>
+                </form>
+                <!-- Tags -->
+                <h5 id="tags">Tags</h5>
+            </div>
+        </nav>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+        <!-- Main Content -->
+        <main class="col-md-9 ">
+           <h1>Main Content</h1>
+        </main>
+    </div>
+</div>
